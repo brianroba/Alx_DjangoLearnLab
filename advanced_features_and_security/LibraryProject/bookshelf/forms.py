4f2a3a7dcd7b44f4e7b6a0f6c5e346d2164a885c
Brian Roba <brianroba89@yahoo.com>
2025-09-14 22:52:19 +0300

    Add ExampleForm for security assignment

--- advanced_features_and_security/LibraryProject/bookshelf/forms.py
+++ advanced_features_and_security/LibraryProject/bookshelf/forms.py
@@ -2,3 +2,8 @@ from django import forms
 
 class BookSearchForm(forms.Form):
     title = forms.CharField(max_length=100)
+
+class ExampleForm(forms.Form):
+    name = forms.CharField(max_length=100)
+    email = forms.EmailField()
+    message = forms.CharField(widget=forms.Textarea)
