1e3a5d5d158b547de39f800ab93b88b367741879
Brian Roba <brianroba89@yahoo.com>
2025-09-14 20:06:03 +0300

    another commit

--- django-models/LibraryProject/relationship_app/views.py
+++ django-models/LibraryProject/relationship_app/views.py
@@ -3,6 +3,7 @@ from .models import Book
 from django.views.generic.detail import DetailView
 from .models import Library
 from django.shortcuts import render, redirect
+from django.contrib.auth.forms import UserCreationForm
 from django.contrib.auth.forms import AuthenticationForm, UserCreationForm
 from django.contrib.auth import login, logout
 
