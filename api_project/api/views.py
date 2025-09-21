3b4e495ab19b3dd1840c18ba67b470b88a103dbf
Brian Roba <brianroba89@yahoo.com>
2025-09-21 22:35:29 +0300

    third commit

--- api_project/api/views.py
+++ api_project/api/views.py
@@ -4,9 +4,16 @@ from django.shortcuts import render
 # api/views.py
 
 from rest_framework import generics
+from rest_framework import viewsets
 from .models import Book
 from .serializers import BookSerializer
 
 class BookList(generics.ListAPIView):
+    queryset = Book.objects.all()
+    serializer_class = BookSerializer
+
+# Existing BookList view can remain or be removed if you want to use only the ViewSet.
+
+class BookViewSet(viewsets.ModelViewSet):
     queryset = Book.objects.all()
     serializer_class = BookSerializer
