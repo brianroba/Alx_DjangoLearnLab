c522fe8b892dad0dc7cb66ceb4795ac3749750bd
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-10-03 20:11:20 +0300

    fourth commit

--- django_blog/blog/urls.py
+++ django_blog/blog/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
 from .views import register_view, CustomLoginView, CustomLogoutView
 from django.contrib.auth import views as auth_views
+from django.conf import settings
+from django.conf.urls.static import static
 
 urlpatterns = [
     path('register/', register_view, name='register'),
     path('login/', CustomLoginView.as_view(), name='login'),
     path('logout/', CustomLogoutView.as_view(), name='logout'),
-]
+] + static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
