d63015dec147f015824195da15778bcd0711d7d9
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-10-03 19:16:06 +0300

    settings.py changes commit

--- django_blog/django_blog/settings.py
+++ django_blog/django_blog/settings.py
@@ -77,6 +77,10 @@ DATABASES = {
     'default': {
         'ENGINE': 'django.db.backends.sqlite3',
         'NAME': BASE_DIR / 'db.sqlite3',
+        'USER': '',     # <-- USER goes here
+        'PASSWORD': '',
+        'HOST': '',
+        'PORT': '',             # <-- PORT goes here
     }
 }
 
