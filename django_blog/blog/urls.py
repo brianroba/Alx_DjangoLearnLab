0dcdc935cc04578e804c8eb68f21b12df1cab34e
BRIAN ROBA ABDUBA <K00015228@kra.gov>
2025-10-03 19:41:05 +0300

    third commit

---
+++ django_blog/blog/urls.py
@@ -0,0 +1,9 @@
+from django.urls import path
+from .views import register_view, CustomLoginView, CustomLogoutView
+from django.contrib.auth import views as auth_views
+
+urlpatterns = [
+    path('register/', register_view, name='register'),
+    path('login/', CustomLoginView.as_view(), name='login'),
+    path('logout/', CustomLogoutView.as_view(), name='logout'),
+]
--- django_blog/blog/views.py
+++ django_blog/blog/views.py
@@ -1,3 +1,29 @@
 from django.shortcuts import render
 
+# blog/views.py
+from django.shortcuts import render, redirect
+from django.contrib.auth import login, logout
+from django.contrib.auth.views import LoginView, LogoutView
+from .forms import CustomUserCreationForm
+
 # Create your views here.
+
+# Registration View
+def register_view(request):
+    if request.method == 'POST':
+        form = CustomUserCreationForm(request.POST)
+        if form.is_valid():
+            user = form.save()
+            login(request, user)  # Log in user after registration
+            return redirect('home')  # Redirect to home or wherever you want
+    else:
+        form = CustomUserCreationForm()
+    return render(request, 'blog/register.html', {'form': form})
+
+# Login View (using built-in)
+class CustomLoginView(LoginView):
+    template_name = 'blog/login.html'
+
+# Logout View (using built-in)
+class CustomLogoutView(LogoutView):
+    next_page = 'login'  # Redirect after logout
